9f8d557e201cd67bda9e45262fe49b471776914b
Eugene Golushkov <e.golushkov@nospam_gmail.com>
2017-09-26 08:41:07 +0300

    [OSX,iOS] Fixed crashes caused by wrong OpenGL context during windowing operations
    Cherry-picked
    Source: https://github.com/OGRECave/ogre
    Commit: 36e4be97741aefd0df8e0e5092116a5d35df28f9

--- RenderSystems/GLSupport/src/OSX/OgreOSXCocoaWindow.mm
+++ RenderSystems/GLSupport/src/OSX/OgreOSXCocoaWindow.mm
@@ -60,6 +60,15 @@ THE SOFTWARE.
 @end
 
 namespace Ogre {
+    
+    struct NSOpenGLContextGuard
+    {
+        NSOpenGLContextGuard(NSOpenGLContext* ctx) : mPrevContext([NSOpenGLContext currentContext]) { if(ctx != mPrevContext) [ctx makeCurrentContext]; }
+        ~NSOpenGLContextGuard() { [mPrevContext makeCurrentContext]; }
+    private:
+         NSOpenGLContext *mPrevContext;
+    };
+
 
     CocoaWindow::CocoaWindow() : mWindow(nil), mView(nil), mGLContext(nil), mGLPixelFormat(nil), mWindowOriginPt(NSZeroPoint),
         mWindowDelegate(NULL), mActive(false), mClosed(false), mVSync(true), mHasResized(false), mIsExternal(false), mWindowTitle(""),
@@ -546,6 +555,9 @@ namespace Ogre {
             mWindowOriginPt = [mWindow frame].origin;
             [mWindow setContentSize:NSMakeSize(widthPt, heightPt)];
         }
+        //make sure the context is current
+        NSOpenGLContextGuard ctx_guard(mGLContext);
+
 		[mGLContext update];
     }
 
@@ -568,6 +580,9 @@ namespace Ogre {
         mTop = _getPixelFromPoint((int)topPt);
 
         mWindowOriginPt = NSMakePoint(leftPt, topPt);
+
+        //make sure the context is current
+        NSOpenGLContextGuard ctx_guard(mGLContext);
 
         for (ViewportList::iterator it = mViewportList.begin(); it != mViewportList.end(); ++it)
         {
